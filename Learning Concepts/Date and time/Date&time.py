c151a5577060a0ed34c7155d340689b85ec8937a
Mishal Mathew <mishalmmathew@gmail.com>
2025-08-31 22:59:33 +0530

    Added info on datetime module
    Added info on how to change string to datetime data type and some additional things too.

--- Learning Concepts/Date and time/Date&time.py
+++ Learning Concepts/Date and time/Date&time.py
@@ -5,8 +5,16 @@ date = datetime.date(day=26,month=4,year=2007)
 today = datetime.date.today()
 
 time = datetime.time(12,17,20)
+
+
+#datetime data type to string
 current_time = datetime.datetime.now()    #datetime is also a class
 current_time = current_time.strftime(f"Current time:%H:%M:%S \n{date}\nCurrent date:{today}\nor\nCurrent date:%d-%m-%y")
 
+#String data type to datetime
+user_string = "22:04:40"
+user_time = datetime.datetime.strptime(user_string , "%H:%M:%S").time() # The .time() returns the time part only while date() gives date only
+
+print(user_time)
 
 print(current_time)
