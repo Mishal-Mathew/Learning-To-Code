e5c31c7251674c8de5e5aae3b776afb89f3a0408
Mishal Mathew <mishalmmathew@gmail.com>
2025-09-01 00:38:08 +0530

    Update Alarm clock.py

--- Learning projects/Alarm clock.py
+++ Learning projects/Alarm clock.py
@@ -7,12 +7,13 @@ def alarm_clock(alarm_time):
     music = "Learning projects\\Claim To Fame - The Grey Room _ Clark Sims.mp3"
     running = True
     while running:
-        current_time = datetime.datetime.now().strftime("%H:%M:%S")
-        print(current_time)
+        current_time = datetime.datetime.now()
+        current_time = current_time.time()
+        print(current_time.strftime("%H:%M:%S"))
         time.sleep(1)
 
         if current_time == alarm_time:
-            running= False
+            running = False
             print("Wake Up!")
             pygame.mixer.init()
             pygame.mixer.music.load(music)
@@ -22,20 +23,22 @@ def alarm_clock(alarm_time):
                 time.sleep(1)
         
 def main():
-    
-    alarm_time = input("Enter the alarm time (HH:MM:SS): ")
-    #alarm_time = datetime.datetime.strptime(alarm_time,"%H:%M:%S").time()
-    current_time=datetime.datetime.now().strftime("%H:%M:%S")
 
-    if alarm_time.isalnum():
-        print("Please enter a valid time in HH:MM:SS")
+    try:
+        alarm_time = input("Enter the alarm time (HH:MM:SS): ")
+        alarm_time = datetime.datetime.strptime(alarm_time,"%H:%M:%S").time()
+        current_time = datetime.datetime.now().time()
 
-    elif alarm_time <= current_time:
-        print("Alarm time should not have passed") 
-    
-    else:
-        alarm_clock(alarm_time)
+        if alarm_time <= current_time:
+            print("The alarm time must not have passed")
+        
+        else:
+            alarm_clock(alarm_time)
+    except ValueError:
+        print("Please enter a valid time")
 
+    except Exception as e:
+        print("Something went wrong",e)    
 
 
 if __name__ == "__main__":
